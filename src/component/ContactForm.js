47fca629db4d1c8e04270d6606cbe4e08a30c9b1
youngchangyoo <ddori5338@hanmail.net>
2024-04-02 00:25:10 +0900

    when submit make input empty

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -5,24 +5,26 @@ import {useDispatch} from 'react-redux';
 
 const ContactForm = () => {
     const [name, setName] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState(0);
+    const [phoneNumber, setPhoneNumber] = useState('');
     const dispatch = useDispatch();
 
     const addContact = (event) => {
         event.preventDefault();
         dispatch({type: "ADD CONTACT", payload: {name, phoneNumber}});
+        setName("");
+        setPhoneNumber("");
     }
 
     return (
         <Form onSubmit={addContact}>
             <Form.Group className="mb-3" controlId="forName">
                 <Form.Label>이름</Form.Label>
-                <Form.Control type="text" placeholder="이름을 입력해주세요" onChange={(event) => setName(event.target.value)}/>
+                <Form.Control type="text" placeholder="이름을 입력해주세요" onChange={(event) => setName(event.target.value)} value={name}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formContact">
                 <Form.Label>전화번호</Form.Label>
-                <Form.Control type="number" placeholder="전화번호를 입력해주세요" onChange={(event) => setPhoneNumber(event.target.value)}/>
+                <Form.Control type="text" placeholder="전화번호를 입력해주세요" onChange={(event) => setPhoneNumber(event.target.value)} value={phoneNumber}/>
             </Form.Group>
 
             <Button variant="primary" type="submit">
